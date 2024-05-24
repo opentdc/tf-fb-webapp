430c23a3a42fdaeaecb8e27194f71c64a5226c56
Bruno Kaiser <bruno@bkaiser.ch>
2024-05-24 14:18:16 +0200

    added License, Readme and .gitignore

--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
+# Copyright [2024] [Bruno Kaiser]
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+# SPDX-License-Identifier: Apache-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 variable "app_id" {
   description = "The app id of the firebase project, 3-5 lowercase chars, followed by 2 digits as a version, separated by a hyphen. e.g. bka."
   type        = string
