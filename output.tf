ad93beb63d3904b339026daaea8f50e25133f1dd
Bruno Kaiser <bruno@bkaiser.ch>
2024-05-24 20:33:48 +0200

    removing the config output; read it from the console instead

--- output.tf
+++ output.tf
@@ -13,20 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-data "google_firebase_web_app_config" "default" {
-  provider     = google-beta
-  project      = var.project_id
-  web_app_id   = var.app_id
-}
-
-output "web_app_config" {
-  value = {
-    projectId         = var.project_id
-    appId             = var.app_id
-    apiKey            = data.google_firebase_web_app_config.default.api_key
-    authDomain        = data.google_firebase_web_app_config.default.auth_domain
-    storageBucket     = lookup(data.google_firebase_web_app_config.default, "storage_bucket", "")
-    messagingSenderId = lookup(data.google_firebase_web_app_config.default, "messaging_sender_id", "")
-    measurementId     = lookup(data.google_firebase_web_app_config.default, "measurement_id", "")
-  }
-}
